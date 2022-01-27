eaebc4b67d12bd8d1cf0d17b2be789b98743740a
kwawrzecki <karol.wawrzecki@imagene.me>
2022-01-27 15:05:04 +0100

    (refactor) remove demo without angular

--- apps/demo/src/plugin-demos/nativescript-google-login.ts
+++
@@ -1,9 +0,0 @@
-import { Observable, EventData, Page } from '@nativescript/core';
-import { DemoSharedNativescriptGoogleLogin } from '@demo/shared';
-
-export function navigatingTo(args: EventData) {
-	const page = <Page>args.object;
-	page.bindingContext = new DemoModel();
-}
-
-export class DemoModel extends DemoSharedNativescriptGoogleLogin {}
