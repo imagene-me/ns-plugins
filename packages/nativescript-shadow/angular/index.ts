b84bd766737127c840ffffe1e4cfdf6cbe47f7e7
kwawrzecki <karol.wawrzecki@imagene.me>
2022-01-28 10:33:38 +0100

    (refactor) fix shadow to new ng-packagr

--- packages/nativescript-shadow/angular/index.ts
+++ packages/nativescript-shadow/angular/index.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NativeShadowDirective } from './ng-shadow.directive';
 
-
 @NgModule({
-  imports: [],
-  declarations: [
-    NativeShadowDirective,
-  ],
-  exports: [
-    NativeShadowDirective,
-  ],
-  providers: [],
+	imports: [],
+	declarations: [NativeShadowDirective],
+	exports: [NativeShadowDirective],
+	providers: [],
 })
-export class NgShadowModule { }
+export class NgShadowModule {}
+export { NativeShadowDirective } from './ng-shadow.directive';
 
 // Uncomment this line if the package provides a custom view component
 // registerElement('NativescriptShadow', () => NativescriptShadow);
